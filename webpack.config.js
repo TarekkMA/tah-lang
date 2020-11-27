8fd4df11e43e50790f603b8094b5ffc24ee0ea34
TarekkMA <tarekkma@gmail.com>
2020-11-27 19:42:51 +0200

    add dev and prod modes

--- webpack.config.js
+++ webpack.config.js
@@ -4,56 +4,59 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 /**
- * @type {import("webpack").Configuration}
+ * @returns {import("webpack").Configuration}
  */
-const config = {
-  mode: 'production',
-  entry: './webapp/index.ts',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'js/[name].[chunkhash:8].js',
-    publicPath: '/tah-lang',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './webapp/index.html',
-    }),
-    new MonacoWebpackPlugin({
-      languages: [],
-    }),
-    new CleanWebpackPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.ttf$/,
-        use: ['file-loader'],
-      },
+const config = (env) => {
+  const isProd = env.PROD != undefined;
+  return {
+    mode: isProd ? 'production' : 'development',
+    entry: './webapp/index.ts',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'js/[name].[chunkhash:8].js',
+      publicPath: isProd ? '/tah-lang' : '/',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './webapp/index.html',
+      }),
+      new MonacoWebpackPlugin({
+        languages: [],
+      }),
+      new CleanWebpackPlugin(),
     ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/i,
-          name: 'vendors',
-          chunks: 'all',
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.ttf$/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/i,
+            name: 'vendors',
+            chunks: 'all',
+          },
         },
       },
     },
-  },
+  };
 };
 
 module.exports = config;
